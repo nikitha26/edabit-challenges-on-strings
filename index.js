b32cce3ba3192f8c83632c7e82653194d3a7b23e
nikitha26 <mnikitha365@gmail.com>
2020-07-26 22:58:11 +0530

    Initial setup

---
+++ index.js
@@ -0,0 +1,49 @@
+
+// //find max and min numbers in an array and returns array 
+// // function fun1(num){
+// // var name = num.sort(function(a,b){
+// //     return a-b;
+// // });
+// //  return [name[0],name[name.length-1]]
+// // }
+// // console.log(fun1([2,3,2,4,6,5,7]));
+
+
+// //find biggest number in the number
+// function add(num){
+//     var add = 0;
+// for(i=num;i>=0;i--)
+// {
+//     add +=i;
+// }
+// console.log(add)
+// }
+// add(3)
+
+
+// //Divides Evenly
+// function even(a,b){
+// if(a%b==0)
+// {
+//     console.log("true")
+// }
+// else{
+//     console.log("false")
+// }
+// }
+// even(15,2)
+
+//Factorial
+
+// function sortNumsAscending(arr) {
+// 	var z = arr.sort(function(a,b){
+// 		return a-b;
+// 	})
+//   console.log(z);
+// }
+// sortNumsAscending([2,4,6,1,3,8])
+
+var num = 0;
+var x = num.toString();
+var y = x.split('');
+console.log(y);
